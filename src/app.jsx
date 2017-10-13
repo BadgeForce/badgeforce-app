66dc32c03edfa7ec057c89d91bd09e7c5538dc1f
Khalil Claybon <khalil.claybon@yodle.com>
2017-10-13 00:24:08 -0400

    use refactored badge-rest-server provided badgeforce web3 client

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import {default as restserver} from 'badge-rest-engine';
+import {default as badgeForce} from 'badge-rest-engine';
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
@@ -6,8 +6,10 @@ import {
   Redirect
 } from 'react-router-dom'
 import Home from './pages/home/home';
-
+const bfClient = new badgeForce.badgeforceClient("http://localhost:8545", "0x0")
+console.log(bfClient);
 class App extends Component {
+
   render() {
     return (
       <div>
@@ -22,4 +24,3 @@ class App extends Component {
   }
 }
 export default App;
-//restserver.run();
